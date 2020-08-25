21af7eec049ed4ed2ed8bd2c418065b490bc1427
Scirius <68830011+Scirius@users.noreply.github.com>
2020-08-25 11:00:50 +0200

    Updates

---
+++ precache-manifest.41d6f76ef6655dbda6260df93e0ad0fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c666ea8fdc66c8daa4513f14b1bf43ce",
+    "url": "/groupwork-playlist/index.html"
+  },
+  {
+    "revision": "dcaf19071f070140e469",
+    "url": "/groupwork-playlist/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "864995ffa062886ff251",
+    "url": "/groupwork-playlist/static/js/2.f83a5f65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/groupwork-playlist/static/js/2.f83a5f65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcaf19071f070140e469",
+    "url": "/groupwork-playlist/static/js/main.273d2a53.chunk.js"
+  },
+  {
+    "revision": "e695f3c579f856cf1bf1",
+    "url": "/groupwork-playlist/static/js/runtime-main.0fec4758.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/groupwork-playlist/static/media/logo.5d5d9eef.svg"
+  }
+]);
